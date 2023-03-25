9a3e901574be94fe229610ce0b47545730cce753
k.soltani <k.soltani@shadzi.club>
2023-03-26 02:14:31 +0330

    new version of route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
-import { Route, Switch,Redirect } from "react-router-dom";
+import { Routes,Route,Navigate } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Redirect from="/" to="/signup"/>
-      </Switch>
+      <Routes>Route
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Navigate to="/signup"/>}/>
+      </Routes>
     </div>
   );
 }
 export default App;
+
+
+  {
+    /* <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Redirect from="/" to="/signup"/>
+      </Switch> */
+  }
